81084b46280dba838f988d84546bbb0741ec7fd1
Wesley Detweiler <wesley.detweiler@garmin.com>
2015-04-02 16:39:39 -0500

    new: abstract and SELECT query builder classes with test execution.

---
+++ query_builder/README.md
@@ -0,0 +1,13 @@
+query_builder
+=============
+
+Purpose
+-------
+The reason for this module is to create a sqlite query builder based on the builder design pattern.
+
+Notes
+-----
+
+To Do
+-----
+- Break classes out of singular builder module.
---
+++ query_builder/builder.php
@@ -0,0 +1,115 @@
+<?php
+
+abstract class AbstractQueryBuilder {
+    abstract function get_query();
+}
+
+abstract class AbstractQueryDirector {
+    abstract function __construct( AbstractQueryBuilder $builder );
+    abstract function build_query();
+    abstract function get_query();
+}
+
+class SelectQuery {
+    private $query = NULL;
+    private $anchor_table = NULL;
+    private $join_tables = [];
+    private $params = [];
+    private $limit = NULL;
+    private $order = NULL;
+    
+    function __construct() {
+    }
+    
+    function set_tables( $tables = NULL ) {
+        if( $tables === NULL )
+            {
+            //WCD - handle error
+            }
+    }
+    
+    function set_params( $params = NULL ) {
+    
+    }
+    
+    function set_limit( $limit = NULL ) {
+    
+    }
+    
+    function set_order( $order ) {
+    
+    }
+    
+    function format_query() {
+    
+    }
+    
+    public function __toString() {
+        return $this->query;
+    }
+}
+
+class SelectQueryBuilder extends AbstractQueryBuilder {
+    private $query = NULL;
+    
+    function __construct() {
+        $this->query = new SelectQuery();
+    }
+    
+    function set_tables( $tables = NULL ) {
+        $this->query->set_tables( $tables );
+    }
+    
+    function set_params( $params = NULL ) {
+        $this->query->set_params( $params );
+    }
+    
+    function set_limit( $limit = NULL ) {
+        $this->query->set_limit( $limit );
+    }
+    
+    function set_order( $order ) {
+    $this->query->set_order( $order );
+    }
+    
+    function format_query() {
+        $this->query->format_query();
+    }
+    
+    function get_query() {
+        return $this->query;
+    }
+}
+
+class SelectQueryDirector extends AbstractQueryDirector {
+    private $builder = NULL;
+    
+    public function __construct( AbstractQueryBuilder $builder ) {
+        $this->builder = $builder;
+    }
+    
+    public function build_query( /* json structure from GET query */) {
+        //set_tables()
+        //etc.
+        $this->builder->format_query()
+    }
+    
+    public function get_query() {
+        return $this->builder->get_query();
+    }
+
+}
+
+/* Test Execution */
+$query_builder = new SelectQueryBuilder();
+$query_director = new SelectQueryDirector( $query_builder );
+$query_director->build_query( /* json structure */ );
+$query = $query_director->get_query();
+echo $query;
+
+
+
+
+
+
+?>
